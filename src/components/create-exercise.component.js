d6c486b1ff69f5f8b477bce5a0efab229ac80136
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-02-20 19:54:49 -0600

    add components

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+export default class CreateExercise extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            username: '',
+            description: '',
+            duartion: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duartion,
+            date: this.state.date
+        }
+        console.log(exercise);
+        window.location = '/';
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Create Exercise Component</p>
+            </div>
+        );
+    }
+}

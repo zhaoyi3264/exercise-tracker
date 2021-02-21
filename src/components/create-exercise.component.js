9307a6080cb1d29778921bc662cfaa9603285873
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-02-21 13:23:00 -0600

    connect to backend

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -52,14 +53,25 @@ export default class CreateExercise extends Component {
             date: this.state.date
         }
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
         window.location = '/';
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['zhaoyi'],
-            username: 'test user'
-        });
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     render() {
